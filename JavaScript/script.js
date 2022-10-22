678f270ad327f0667a99190fb846d73a98e1e957
MelanyBepo <115649633+MelanyBepo@users.noreply.github.com>
2022-10-21 19:02:28 -0600

    update1021

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -297,20 +297,3 @@ accordionHeaderEL.forEach((accordionHeaderEL) => {
     }
   });
 });
-
-/*Swiper Configuration*/
-var swiper = new swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
